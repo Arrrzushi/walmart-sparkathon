df40d2ea8a74df4d4844f33fa9e165f86d2dc0fd
Prthmsh-RustBucket <prathmeshshukla2106@gmail.com>
2025-07-14 20:53:30 +0530

    added the base project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 className="cool-heading">Hello Walmart</h1>
+    </div>
+  )
+}
+
+export default App
